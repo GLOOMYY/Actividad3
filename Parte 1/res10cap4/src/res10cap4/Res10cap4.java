87ffac842051c4d972d3f153554dec65e7e70692
GLOOMYY <sebas.mesa.montoya@gmail.com>
2023-05-22 17:07:42 -0500

    Resuelto 10

---
+++ Parte 1/res10cap4/src/res10cap4/Res10cap4.java
@@ -0,0 +1,11 @@
+package res10cap4;
+
+
+public class Res10cap4 {
+
+    public static void main(String[] args) {
+        interfaz ventana = new interfaz();
+        ventana.setVisible(true);
+    }
+    
+}
