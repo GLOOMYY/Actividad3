83c7486afdbcce47392c5d463f0fd46ff079b5f4
GLOOMYY <sebas.mesa.montoya@gmail.com>
2023-05-22 12:01:36 -0500

    Propuesto 19

---
+++ Parte 1/prop19cap3/src/prop19cap3/Prop19cap3.java
@@ -0,0 +1,12 @@
+
+package prop19cap3;
+
+
+public class Prop19cap3 {
+
+    public static void main(String[] args) {
+        interfaz ventana = new interfaz();
+        ventana.setVisible(true);
+    }
+    
+}
