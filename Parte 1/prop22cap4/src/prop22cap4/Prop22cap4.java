8a429c2de018162a0fa5223eedee3d22cde37caa
GLOOMYY <sebas.mesa.montoya@gmail.com>
2023-05-22 21:29:49 -0500

    Propuesto 22

---
+++ Parte 1/prop22cap4/src/prop22cap4/Prop22cap4.java
@@ -0,0 +1,8 @@
+package prop22cap4;
+
+public class Prop22cap4 {
+    public static void main(String[] args) {
+        interfaz ventana = new interfaz();
+        ventana.setVisible(true);
+    }
+}
